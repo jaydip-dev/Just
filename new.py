55435d4b6eb8e52ceced335231ae78614bbbc1bc
Jaydip Prajapati <jaydipp@webelight.co.in>
2025-08-29 09:25:40 +0530

    Updated new.py some internal changes

--- new.py
+++ new.py
@@ -1,8 +1,10 @@
 def sum(x,y):
-    print(f'Sum of {x / y}') 
-def mult(x,y):
     print(f'Sum of {x + y}') 
-
+def mult(x,y):
+    print(f'Mult of {x * y}') 
+def dev(x,y):
+    print(f'dev of {x / y)')
 print("made a new function for multification")
-
+sum(x,y)
 mult(10,30)
+dev(x,y)
